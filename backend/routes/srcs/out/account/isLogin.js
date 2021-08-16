a08e89edf96a6ac28dcc4db49d34fcb20f17e084
featherchen <garychen0975321741@gmail.com>
2021-08-16 16:11:11 +0800

    return userimage in isLogin

--- backend/routes/srcs/out/account/isLogin.js
+++ backend/routes/srcs/out/account/isLogin.js
@@ -1,4 +1,4 @@
-const { ErrorHandler } = require("../../../error")
+const { ErrorHandler } = require('../../../error')
 
 /**
  * @api {post} /isLogin isLogin
@@ -7,11 +7,13 @@ const { ErrorHandler } = require("../../../error")
  * @apiDescription 檢查是否有登入
  *
  * @apiSuccess (201) {String} account 登入者學號
- * 
+ *
  * @apiError (403) {String} description "未登入"
  */
 module.exports = (req, res, next) => {
-    const session_account = req.session.loginAccount
-    if(session_account) return res.status(200).send({account:session_account})
-    else throw new ErrorHandler(403,'未登入')
+  const session_account = req.session.loginAccount
+  const session_userimage = req.session.userimage
+  if (session_account)
+    return res.status(200).send({ account: session_account, userimage: session_userimage })
+  else throw new ErrorHandler(403, '未登入')
 }
